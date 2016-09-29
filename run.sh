daa4e535afa3e6f41658dcc04fbf3c18630dda5a
sameer-pandit <getsam1234@gmail.com>
2016-09-30 00:00:15 +0530

    run on linux changes

--- run.sh
+++ run.sh
@@ -6,10 +6,11 @@ set -e
 mvn clean install
 
 # Export the active docker machine IP
-export DOCKER_IP=$(docker-machine ip $(docker-machine active))
+#export DOCKER_IP=$(docker-machine ip $(docker-machine active))
 
 # docker-machine doesn't exist in Linux, assign default ip if it's not set
-DOCKER_IP=${DOCKER_IP:-0.0.0.0}
+fqdn=`hostname -f`
+export DOCKER_IP=$fqdn
 
 # Remove existing containers
 docker-compose stop
